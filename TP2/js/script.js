65842f3cd52f16cb8b78db72fc815018de2f6823
andresacostalautaro <l.andresacosta00@gmail.com>
2024-09-30 23:48:21 -0300

    breadcrums agregado

--- TP2/js/script.js
+++ TP2/js/script.js
@@ -1,3 +1,17 @@
+//Funcionalidad del Breadcrums, cambia a partir del String(palabra) dado ==>
+function setBreadcrumbs(palabra) {
+    const bcsContent = document.getElementById("bcs_content");
+    if (palabra.toLowerCase() === "home" || palabra.toLowerCase() === "log in") {
+        bcsContent.innerHTML = `<h3>${palabra}</h3>`;
+    } else {
+        bcsContent.innerHTML = `
+            <h3>Category</h3>
+            <img src="breadcrums-greater-than.svg" alt=">" class="breadcrumbs_greaterthan_icon">
+            <h3>${palabra}</h3>
+        `;
+    }
+}//Funcionalidad del Breadcrums <==
+
 // Funcionalidad del formulario Log In - frame("form_LogIn") ==>
 document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault();
@@ -11,25 +25,20 @@ document.getElementById('login-form').addEventListener('submit', function(event)
         console.log('Please fill in all fields');
     }
 });
-
 document.querySelector('.google-button').addEventListener('click', function() {
     // Aquí puedes añadir la funcionalidad de Google Sign-In
     console.log('Google Sign-In');
 });
-
 document.querySelector('.facebook-button').addEventListener('click', function() {
     // Aquí puedes añadir la funcionalidad de Facebook Sign-In
     console.log('Facebook Sign-In');
 });
-
 function handleCredentialResponse(response) {
     decodeJwtResponse(response.credential);
 }
-
 function decodeJwtResponse(data) {
     console.log(parseJwt(data));
 }
-
 function parseJwt(token) {
     var base64Url = token.split(".")[1];
     var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
@@ -44,5 +53,4 @@ function parseJwt(token) {
     );
 
     return JSON.parse(jsonPayload);
-}
-// Funcionalidad del formulario Log In - frame("form_LogIn") <==
+}// Funcionalidad del formulario Log In - frame("form_LogIn") <==
