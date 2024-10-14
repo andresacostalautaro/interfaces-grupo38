b8884cb3849be78b2ed7032c2070a3b74a8b13ae
andresacostalautaro <l.andresacosta00@gmail.com>
2024-10-14 17:15:05 -0300

    cambios generales en loading y gameDetails

--- TP2/js/breacrums.js
+++ TP2/js/breacrums.js
@@ -3,8 +3,9 @@ export function setBreadcrumbs(palabra) {
     if (palabra.toLowerCase() === "home" || palabra.toLowerCase() === "sign in" || palabra.toLowerCase() === "sign up" || palabra.toLowerCase() === "log in") {
         bcsContent.innerHTML = `<h3>${palabra}</h3>`;
     } else {
+        palabra = palabra.replace("aventura", '');
         bcsContent.innerHTML = `
-            <h3>Categoria</h3>
+            <h3>Aventura</h3>
             <img src="assets/images/breadcrums-greater-than.svg" alt=">" class="breadcrumbs_greaterthan_icon">
             <h3>${palabra}</h3>
         `;
